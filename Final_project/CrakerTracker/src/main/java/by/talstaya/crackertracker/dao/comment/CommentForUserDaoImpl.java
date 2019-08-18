acc57deefe60bbfae668b548338ac9c7737084fe
Anna Talstaya <an.t98@yandex.ru>
2019-08-18 21:04:01 +0300

    Updated.

--- Final_project/CrakerTracker/src/main/java/by/talstaya/crackertracker/dao/comment/CommentForUserDaoImpl.java
+++ Final_project/CrakerTracker/src/main/java/by/talstaya/crackertracker/dao/comment/CommentForUserDaoImpl.java
@@ -8,6 +8,8 @@ import by.talstaya.crackertracker.exception.DaoException;
 import by.talstaya.crackertracker.exception.ServiceException;
 import by.talstaya.crackertracker.service.UserService;
 import by.talstaya.crackertracker.service.impl.UserServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -22,12 +24,22 @@ import java.util.List;
  */
 public class CommentForUserDaoImpl implements CommentForUserDao {
 
+    private static final Logger LOGGER = LogManager.getLogger("name");
+
     private static final String SQL_FIND_COMMENTS = "SELECT id, date_of_comment, mealDate, user_id, commentator_id, comment" +
             " FROM comments_for_users" +
             " WHERE user_id=? AND mealDate=?";
 
     private static final String SQL_INSERT_COMMENT = "INSERT into comments_for_users (date_of_comment, mealDate, user_id, commentator_id, comment)" +
             " VALUES (NOW(),?,?,?,?)";
+
+    private static final String SQL_UPDATE = "UPDATE comments_for_users" +
+            " SET date_of_comment=?, mealDate=?, user_id=?, commentator_id=?, comment=?" +
+            " WHERE id=?";
+
+    private static final String SQL_FIND_COMMENT_BY_ID =
+            "SELECT id, date_of_comment, mealDate, user_id, commentator_id, comment" +
+            " FROM comments_for_users WHERE id=?";
 
     private static final String SQL_DELETE_COMMENT = "DELETE FROM comments_for_users WHERE id=?";
 
@@ -179,14 +191,68 @@ public class CommentForUserDaoImpl implements CommentForUserDao {
     }
 
     @Override
-    public void update(CommentForUser entity) throws DaoException {
-        //todo
+    public void update(CommentForUser commentForUser) throws DaoException {
+        PreparedStatement preparedStatement = null;
+        Connection connection = ConnectionPool.getInstance().takeConnection();
+
+        try {
+            preparedStatement = connection.prepareStatement(SQL_UPDATE);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(commentForUser.getDateOfComment()));
+            preparedStatement.setDate(2, Date.valueOf(commentForUser.getMealDate()));
+            preparedStatement.setInt(3, commentForUser.getUserId());
+            preparedStatement.setInt(4, commentForUser.getCommentator().getUserId());
+            preparedStatement.setString(5, commentForUser.getComment());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        } finally {
+            closePreparedStatement(preparedStatement);
+            ConnectionPool.getInstance().returnConnection(connection);
+        }
     }
 
     @Override
     public CommentForUser findById(int id) throws DaoException {
-        return null; //todo
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        Connection connection = ConnectionPool.getInstance().takeConnection();
+        try {
+            preparedStatement = connection.prepareStatement(SQL_FIND_COMMENT_BY_ID);
+            preparedStatement.setInt(1, id);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+
+                return new CommentForUser.Builder()
+                        .setCommentId(resultSet.getInt(1))
+                        .setDateOfComment(resultSet.getTimestamp(2).toLocalDateTime())
+                        .setMealDate(resultSet.getDate(3).toLocalDate())
+                        .setUserId(resultSet.getInt(4))
+                        .setCommentator(findUserById(resultSet.getInt(5)))
+                        .setComment(resultSet.getString(6))
+                        .build();
+            } else {
+                throw new DaoException("No comment with such id");
+            }
+
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        } catch (ServiceException e) {
+            LOGGER.error(e);
+            throw new DaoException(e);
+        } finally {
+            closeResultSet(resultSet);
+            closePreparedStatement(preparedStatement);
+            ConnectionPool.getInstance().returnConnection(connection);
+        }
     }
 
+    private User findUserById(int userId) throws ServiceException {
+        UserService userService = new UserServiceImpl();
+        return userService.findUserById(userId);
+    }
 
 }
